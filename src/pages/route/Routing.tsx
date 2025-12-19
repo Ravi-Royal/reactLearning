8da47fd5efe533674160e1b6eb0320898096c06a
Ravi-Royal-143 <81796854+Ravi-Royal-143@users.noreply.github.com>
2025-12-19 21:28:03 +0530

    feat: Refactor routing to use lazy loading for all components

--- src/pages/route/Routing.tsx
+++ src/pages/route/Routing.tsx
@@ -1,61 +1,76 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Navigate, Outlet, Route, Routes } from "react-router-dom";
-import AboutPage from "../About";
-import HomePage from "../Home";
-import UseCallbackHook from "../components/hookRef/UseCallbackHook";
-import UseContextHook from "../components/hookRef/UseContextHook";
-import UseEffectHook from "../components/hookRef/UseEffectHook";
-import UseReducerHook from "../components/hookRef/UseReducerHook";
-import UseRefHook from "../components/hookRef/UseRefHook";
-import UseStateHook from "../components/hookRef/UseStateHook";
-import Bonds from "../components/investment/bonds/Bonds";
-import BeforeStartingBondCheckList from "../components/investment/bonds/before-starting/BeforeStartingBondCheckList";
-import BondCheckList from "../components/investment/bonds/checklist/BondCheckList";
-import StockAnalysis from "../components/investment/stock/analysis/Analysis";
-import StockResult from "../components/investment/stock/analysis/zerodha/StockResult";
-import StockCheckList from "../components/investment/stock/checklist/StockCheckList";
-import MyFavStocks from "../components/investment/stock/favorites/MyFavStocks";
 import BaseNavigation from "../navigation/BaseNavigation";
-import HooksNavigation from "../navigation/HooksNavigation";
-import InvestmentNavigation from "../navigation/InvestmentNavigation";
-import StockNavigation from "../navigation/StockNavigation";
+
+const HomePage = lazy(() => import('../Home'));
+const AboutPage = lazy(() => import('../About'));
+
+const HooksNavigation = lazy(() => import('../navigation/HooksNavigation'));
+const InvestmentNavigation = lazy(() => import('../navigation/InvestmentNavigation'));
+const StockNavigation = lazy(() => import('../navigation/StockNavigation'));
+
+const UseStateHook = lazy(() => import('../components/hookRef/UseStateHook'));
+const UseEffectHook = lazy(() => import('../components/hookRef/UseEffectHook'));
+const UseRefHook = lazy(() => import('../components/hookRef/UseRefHook'));
+const UseReducerHook = lazy(() => import('../components/hookRef/UseReducerHook'));
+const UseContextHook = lazy(() => import('../components/hookRef/UseContextHook'));
+const UseCallbackHook = lazy(() => import('../components/hookRef/UseCallbackHook'));
+
+const Bonds = lazy(() => import('../components/investment/bonds/Bonds'));
+const BeforeStartingBondCheckList = lazy(() => import('../components/investment/bonds/before-starting/BeforeStartingBondCheckList'));
+const BondCheckList = lazy(() => import('../components/investment/bonds/checklist/BondCheckList'));
+
+const StockAnalysis = lazy(() => import('../components/investment/stock/analysis/Analysis'));
+const StockResult = lazy(() => import('../components/investment/stock/analysis/zerodha/StockResult'));
+const StockCheckList = lazy(() => import('../components/investment/stock/checklist/StockCheckList'));
+const MyFavStocks = lazy(() => import('../components/investment/stock/favorites/MyFavStocks'));
+
+const RouteFallback = () => (
+    <div className="p-6">
+        <div className="text-gray-600">Loading…</div>
+    </div>
+);
 
 function Routing() {
-    return <BrowserRouter>
-        <BaseNavigation />
-        <Routes>
-            <Route path="/" element={<Navigate to="/home" replace />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/hooks" element={<Outlet />}>
-                <Route index element={<HooksNavigation />} />
-                <Route path="useState" element={<UseStateHook />} />
-                <Route path="useEffect" element={<UseEffectHook />} />
-                <Route path="useRef" element={<UseRefHook />} />
-                <Route path="useReducer" element={<UseReducerHook />} />
-                <Route path="useContext" element={<UseContextHook />} />
-                <Route path="useCallback" element={<UseCallbackHook />} />
-            </Route>
-            <Route path="/investment" element={<Outlet />}>
-                <Route index element={<InvestmentNavigation />} />
-                <Route path="stock" element={<Outlet />}>
-                    <Route index element={<StockNavigation />} />
-                    <Route path="analysis" element={<Outlet />}>
-                        <Route index element={<StockAnalysis />} />
-                        <Route path="zerodha" element={<StockResult />} />
+    return (
+        <BrowserRouter>
+            <BaseNavigation />
+            <Suspense fallback={<RouteFallback />}>
+                <Routes>
+                    <Route path="/" element={<Navigate to="/home" replace />} />
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/hooks" element={<Outlet />}>
+                        <Route index element={<HooksNavigation />} />
+                        <Route path="useState" element={<UseStateHook />} />
+                        <Route path="useEffect" element={<UseEffectHook />} />
+                        <Route path="useRef" element={<UseRefHook />} />
+                        <Route path="useReducer" element={<UseReducerHook />} />
+                        <Route path="useContext" element={<UseContextHook />} />
+                        <Route path="useCallback" element={<UseCallbackHook />} />
                     </Route>
-                    <Route path="favorites" element={<MyFavStocks />} />
-                    <Route path="checklist" element={<StockCheckList />} />
-                </Route>
-                <Route path="bonds" element={<Outlet />}>
-                    <Route index element={<Bonds />} />
-                    <Route path="before-starting" element={<BeforeStartingBondCheckList />} />
-                    <Route path="checklist" element={<BondCheckList />} />
-                </Route>
-            </Route>
-            <Route path="*" element={<Navigate to="/home" replace />} />
-
-        </Routes>
-    </BrowserRouter>;
+                    <Route path="/investment" element={<Outlet />}>
+                        <Route index element={<InvestmentNavigation />} />
+                        <Route path="stock" element={<Outlet />}>
+                            <Route index element={<StockNavigation />} />
+                            <Route path="analysis" element={<Outlet />}>
+                                <Route index element={<StockAnalysis />} />
+                                <Route path="zerodha" element={<StockResult />} />
+                            </Route>
+                            <Route path="favorites" element={<MyFavStocks />} />
+                            <Route path="checklist" element={<StockCheckList />} />
+                        </Route>
+                        <Route path="bonds" element={<Outlet />}>
+                            <Route index element={<Bonds />} />
+                            <Route path="before-starting" element={<BeforeStartingBondCheckList />} />
+                            <Route path="checklist" element={<BondCheckList />} />
+                        </Route>
+                    </Route>
+                    <Route path="*" element={<Navigate to="/home" replace />} />
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
+    );
 }
 
 export default Routing;
